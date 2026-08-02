c26fa7fdd34d446bb351c83573a90835a628e285
b0az- <boazobondo205@gmail.com>
2026-08-02 21:16:16 +0300

    feat(run): add C++ support and fix compile/run logic
    - Added  .cpp file detection with g++ compilation
    _ Runs compiled binary only if compilation succeeds, else reports failure
    - Adds self install to path via ~/bashrc
    
    Known issue: PATH self-install only targets ~/.bashrc
    Fish and zsh shell support(TODO)

--- dev/run.sh
+++ dev/run.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+if ! grep -q "SCRIPT_DIR" ~/.bashrc; then
+  echo "export PATH=\"\$PATH:$SCRIPT_DIR\"" >> ~/.bashrc
+  echo "Added run.sh to PATH. Run: source ~/.bashrc (or restart your terminal)"
+fi
+
 file=$1
 output=""
 if [[ $file == *.c ]]; then
   gcc "$file" -o output
-  if [ &? -eq 0 ]; then
+
+  if [ $? -eq 0 ]; then
     ./output
   else
     echo "Compilation failed"
   fi
+
+elif [[ $file == *.cpp ]]; then
+  g++ "file" -o output
+
+  if [ $? -eq 0 ]; then
+    ./output
+  else
+    echo "Compilation failed"
+  fi
+
 else
   echo "Unsupported file type: $file"
 fi
