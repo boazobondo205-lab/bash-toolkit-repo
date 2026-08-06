8af78d6096ec45a81c0563bbc95262ecb6acf7b3
b0az- <boazobondo205@gmail.com>
2026-08-07 00:29:41 +0300

    add .gitignore

--- system/sysinfo.sh
+++ system/sysinfo.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+if ! grep -q "SCRIPT_DIR" ~/.bashrc; then
+  echo "export PATH=\"\$PATH:$SCRIPT_DIR\"" >> ~/.bashrc
+  fi
 echo "[IDENTITY]"
 echo "User: $(whoami)"
 echo "Hostname: $(hostname)"
